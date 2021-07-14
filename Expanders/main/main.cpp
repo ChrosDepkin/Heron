560876b985b747bd536c1bc9df06cf74a0bb2c00
ChrosDepkin <38392605+ChrosDepkin@users.noreply.github.com>
2021-07-14 14:02:31 +1000

    Button Expander Code

---
+++ Expanders/main/main.cpp
@@ -0,0 +1,59 @@
+
+extern "C" {                    // Need to put these includes in here to make it work
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+void app_main(void);           // main needs to be in here too or it won't compile
+}
+
+#include "expanders.h"         // Deals with IO Expanders
+#include "I2Cdefs.h"           // Just defines for various I2C stuff
+
+void I2C_Config();
+
+void app_main()
+{
+    I2C_Config();   // Sets up I2C Master and Slave (only master used here)
+    MCP MCP_E(EXP_ADR_E, MCP_DEF_CONFIG, DIR_PA_E, DIR_PB_E, PU_PA_E, PU_PB_E);     // Expander and misc. expanders currently base class
+    MCP MCP_M(EXP_ADR_M, MCP_DEF_CONFIG, DIR_PA_M, DIR_PB_M, PU_PA_M, PU_PB_M);
+    MCPB MCP_B(EXP_ADR_B, MCP_DEF_CONFIG, DIR_PA_B, DIR_PB_B, PU_PA_B, PU_PB_B);    // Button expander is a child class
+}
+
+
+// Not sure exactly where I2C config function should go - not that far into organising the project yet
+void I2C_Config()
+{
+    //Below I create two configs for a master and a slave I2C
+    i2c_config_t i2cM; //Config structure for first I2C
+        i2cM.mode = I2C_MODE_MASTER; //Set to master
+        i2cM.sda_io_num = I2C_M_SDA; //SDA pin 
+        i2cM.scl_io_num = I2C_M_SCL; //SCL pin 
+        i2cM.sda_pullup_en = GPIO_PULLUP_ENABLE; //Enable internal pullup on SDA pin
+        i2cM.scl_pullup_en = GPIO_PULLUP_ENABLE; //Enable internal pullup on SCL pin
+        i2cM.master.clk_speed = I2C_SPEED; //Clock speed on SCL
+        //Don't set address mode or set slave address in master mode
+
+    i2c_param_config(I2C_M_PORT,&i2cM); //Configure I2C Port 0 with the above config - Port 0 will be our master
+
+    i2c_config_t i2cS; //Config structure for second I2C
+        i2cS.mode = I2C_MODE_SLAVE; //Set to slave
+        i2cS.sda_io_num = I2C_S_SDA; //SDA pin 
+        i2cS.scl_io_num = I2C_S_SCL; //SCL pin 
+        i2cS.sda_pullup_en = GPIO_PULLUP_ENABLE; //Enable internal pullup on SDA pin
+        i2cS.scl_pullup_en = GPIO_PULLUP_ENABLE; //Enable internal pullup on SCL pin
+        i2cS.slave.addr_10bit_en = 0; //Do we want 10 bit address mode?
+        i2cS.slave.slave_addr = I2C_S_ADR; //Set our slave address
+        //Don't set clock speed as slave
+
+    i2c_param_config(I2C_S_PORT,&i2cS); //Configure I2C Port 1 with the above config - Port 1 will be our slave
+
+    //Can also set a few other default values, most relevent probably lsb/msb first, and timeout
+    //See: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/i2c.html#i2c-api-customized-configuration
+
+    i2c_driver_install(I2C_M_PORT,I2C_MODE_MASTER, 0, 0, 0); //Finally install driver for Port 0 - would also include flags for interrupt here (set to 0 for this example)
+                    //Port, Mode, RX Buffer, TX Buffer, Flag
+    i2c_driver_install(I2C_S_PORT,I2C_MODE_SLAVE, 1, 1, 0); //And driver for Port 1 - As its slave it needs (or should have) buffers
+                                                //Just set buffers to 1 for example.
+}
