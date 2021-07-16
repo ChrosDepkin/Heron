1b224d63e16e4f3c4fe80c7d2359647b102f9347
ChrosDepkin <38392605+ChrosDepkin@users.noreply.github.com>
2021-07-16 15:39:37 +1000

    Testing LEDs

---
+++ LEDs/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+#include <FastLED.h>
+#define NUM_LEDS 73
+#define DATA_PIN 12
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
+
+  
+}
+
+void loop() {
+      
+  for (int i = 0; i < 73; i++)
+  {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(30); 
+  }
+  for (int i = 0; i < 73; i++)
+  {
+    leds[i] = CRGB::Black;
+    FastLED.show();
+    delay(30); 
+  }
+
+}
