95afd0dfcbd3ded849efa08053e14e120f7eac63
ChrosDepkin <38392605+ChrosDepkin@users.noreply.github.com>
2021-07-17 17:20:15 +1000

    More board testing

--- LEDs/src/main.cpp
+++ LEDs/src/main.cpp
@@ -7,25 +7,33 @@
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
-
+  FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setCorrection(UncorrectedColor);
+  FastLED.setTemperature(UncorrectedTemperature);
+  FastLED.setDither(DISABLE_DITHER);
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = CRGB::Blue;
+  }
+    FastLED.setBrightness(50);
+    FastLED.show();
   
 }
 
 void loop() {
       
-  for (int i = 0; i < 73; i++)
-  {
-    leds[i] = CRGB::Red;
-    //FastLED.setBrightness(50);
-    FastLED.show();
-    delay(30); 
-  }
-  for (int i = 0; i < 73; i++)
-  {
-    leds[i] = CRGB::Black;
-    FastLED.show();
-    delay(30); 
-  }
 
+/*
+  for (int i = 0; i < 73; i++)
+  {
+    if(i == 0)
+    {leds[72] = CRGB::Black;}
+    if(i > 0)
+    {leds[i-1] = CRGB::Black;}
+    leds[i] = CRGB::Green;
+    FastLED.setBrightness(50);
+    FastLED.show();
+    delay(100); 
+  }
+*/
 }
